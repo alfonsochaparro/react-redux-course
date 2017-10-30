d78d8b782d90d1f29fd3c81345a0ada036551026
Alfonso <alfonso.chaparro.m@gmail.com>
2017-10-30 12:37:32 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const App = function() {
-	return <div>Hi!</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCCGZI6kQak78zVXCgKL2xHSlQ2Ssdcisw';
+
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
